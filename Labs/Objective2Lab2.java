0c1872482de351481a887e5981b701d10043e5d9
Tylor Shearn <tylorshearn95@gmail.com>
2020-11-28 15:46:48 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,19 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Tylor Shearn";
+    int age = 24;
+    String state = "Florida";
+
+    System.out.print("Name: ");
+    System.out.print(name);
+    System.out.print(" ");
+    System.out.print("Age: ");
+    System.out.print(age);
+    System.out.print(" ");
+    System.out.print("State: ");
+    System.out.print(state);
+    System.out.println(" ");
+
+
+  }
+}
