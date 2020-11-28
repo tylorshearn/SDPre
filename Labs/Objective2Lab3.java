81ca6a5a742e5989c8c44deae772367749dc7ec3
Tylor Shearn <tylorshearn95@gmail.com>
2020-11-28 15:56:21 -0500

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String myVariable = "Taco";
+    System.out.println(myVariable);
+    myVariable = "Hotdog";
+    System.out.println(myVariable);
+  }
+}
