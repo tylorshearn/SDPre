37c1482e867042095c7ec5266ba11ae7bd05dcf8
Tylor Shearn <tylorshearn95@gmail.com>
2020-11-28 14:38:58 -0500

    complete Objective1Lab2

--- Labs/Objective1Lab2.java
+++ Labs/Objective1Lab2.java
@@ -1,10 +1,19 @@
 public class Objective1Lab2 {
   public static void main(String[] args) {
+<<<<<<< HEAD
     System.out.println(" ----------------");
     System.out.println("|Grocery List    |");
     System.out.println("|1. Milk         |");
     System.out.println("|2. Eggs         |");
     System.out.println("|3. Bread        |");
     System.out.println(" ----------------");
+=======
+    System.out.println("----------------");
+    System.out.println("Grocery List");
+    System.out.println("1. Milk");
+    System.out.println("2. Eggs");
+    System.out.println("3. Bread");
+    System.out.println("----------------");
+>>>>>>> 5cae794 (complete Objective1Lab2)
   }
 }
