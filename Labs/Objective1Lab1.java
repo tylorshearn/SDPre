287573d380599a602cbaa7b591a5223eb3acd9f3
Tylor Shearn <tylorshearn95@gmail.com>
2020-11-28 14:26:49 -0500

    complete Objective1Lab1

--- Labs/Objective1Lab1.java
+++ Labs/Objective1Lab1.java
@@ -1,9 +1,6 @@
 public class Objective1Lab1 {
   public static void main(String[] args) {
-<<<<<<< HEAD
     System.out.println("Hello, Skill Distillery");
-=======
-    System.out.println("Hello, World");
->>>>>>> c02327f (complete Objective1Lab1)
+    System.out.println("Hello, Skill Distillery");
   }
 }
