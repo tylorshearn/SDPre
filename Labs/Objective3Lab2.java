c3130c23ef6eeecc2433170fc79fed3f341b488a
Tylor Shearn <tylorshearn95@gmail.com>
2020-11-30 16:37:51 -0500

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    side1 = 10.0;
+    side2 = 8.0;
+    hypotenuse = Math.sqrt((side1 * side1) + (side2 * side2));
+
+
+    System.out.println(hypotenuse);
+
+  }
+}
