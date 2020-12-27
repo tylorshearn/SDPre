e99b5ce8118b0e210f63df04604fd4a1cd577042
Tylor Shearn <tylorshearn95@gmail.com>
2020-12-27 03:38:36 -0500

    complete Objective8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,17 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+    for (int a = 1; a <=20; a++) {
+    counter = a;
+
+
+
+    if (counter % 2 == 0) {
+      System.out.println(counter + " is even");
+    }
+   else if (counter % 1 == 0) {
+     System.out.println(counter + " is odd");
+   }
+ }
+  }
+}
