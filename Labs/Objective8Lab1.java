e4ab2722586bfdfbcb6d5c3d018c47b05e1caaed
Tylor Shearn <tylorshearn95@gmail.com>
2020-12-27 02:53:45 -0500

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+      for (int i = 0; i <= 10; i+=1) {
+      System.out.println(i);
+      }
+      for (int iI = 10; iI >=1; iI--) {
+      System.out.println(iI);
+      }
+  }
+}
