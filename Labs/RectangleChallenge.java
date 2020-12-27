b79b42d6997ad8638b76d63a45340d89f9bedf9f
Tylor Shearn <tylorshearn95@gmail.com>
2020-12-27 14:12:10 -0500

    RectangleChallenge

---
+++ Labs/RectangleChallenge.java
@@ -0,0 +1,17 @@
+public class RectangleChallenge {
+    public static void main(String[] args) {
+     printRectangle();
+    }
+
+    public static void printRectangle() {
+      for(int a = 0; a < 10; a++) {
+        System.out.println("XXXXXXXXXX");
+      }
+
+      int a = 0;
+      while (++a < 10) {
+       System.out.println("XXXXXXXXXX");
+      }
+
+  }
+}
