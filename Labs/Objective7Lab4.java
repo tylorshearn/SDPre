5d9f764628b57c895eac47965504b0055dcf94b1
Tylor Shearn <tylorshearn95@gmail.com>
2020-12-27 01:03:32 -0500

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,23 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int sumTotal, counter;
+    sumTotal = 0;
+    counter = 0;
+
+    while (counter <= 20) {
+      sumTotal = sumTotal + counter;
+      ++counter;
+    }
+    System.out.println(sumTotal);
+
+
+
+
+
+
+
+
+
+
+  }
+}
