fe9724d0b5ab4cb555a948f5dca026e8e6120a6d
Tylor Shearn <tylorshearn95@gmail.com>
2020-12-26 22:38:28 -0500

    complete Objective7Lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,24 @@
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    do {
+      if (counter % 2 == 0) {
+        System.out.println(counter + " is even");
+        }
+        else if (counter % 1 == 0) {
+          System.out.println(counter + " is odd");
+        }
+       counter = counter + 1;
+       }  while (counter <= 20);
+
+
+
+
+
+
+
+
+
+  }
+}
