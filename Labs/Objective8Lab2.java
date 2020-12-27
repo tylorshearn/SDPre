7338011a297e53699a7601909afdae87fc0123a5
Tylor Shearn <tylorshearn95@gmail.com>
2020-12-27 03:16:32 -0500

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,9 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sumTotal = 0;
+    for (int a = 0; a <=20; a ++) {
+    sumTotal = sumTotal + a;
+   }
+    System.out.println(sumTotal);
+  }
+}
